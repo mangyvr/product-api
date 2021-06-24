699caa0540185fe0786acb01a7a38eb60486fd38
Tsung-Yen Tsai <tsung-yen.tsai@mail.mcgill.ca>
2021-06-23 21:51:10 -0700

    update counters, most viewed

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::ProductsController < ApplicationController
   def show
     begin
       product = Product.not_deleted.find(params[:id])
+
+      # update_counters uses a SQL update statement to directly update the view count
+      # so should be accurate still for writes that are concurrent/waiting for lock
+      Product.update_counters(product.id, view_count: 1)
       render json: product, status: :ok
     rescue ActiveRecord::RecordNotFound => e
       head :not_found
@@ -20,7 +24,9 @@ class Api::V1::ProductsController < ApplicationController
 
   def create
     begin
-      return render json: {message: 'Price is required.'}, status: :bad_request if params[:price].nil?
+      if params[:price].nil?
+        return render json: {message: 'Validation failed: Price is required.'}, status: :bad_request
+      end
 
       price_in_cents = (params[:price] * 100).to_i
       product = Product.create!(price: price_in_cents, 
@@ -34,6 +40,16 @@ class Api::V1::ProductsController < ApplicationController
   end
 
   def most_viewed
+    if params[limit] && params_limit < 0
+      return render json: {message: 'Limit must be positive'}, status: :bad_request
+    end
 
+    max_num_products = params[limit] || 5
+    products = Product.not_deleted
+                      .with_views
+                      .order(view_count: :desc)
+                      .limit(max_num_products)
+
+    render json: products, status: :ok
   end
 end

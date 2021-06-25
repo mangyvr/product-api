74753137b454ffc6ed0198bf27b154bab17c7565
Tsung-Yen Tsai <tsung-yen.tsai@mail.mcgill.ca>
2021-06-24 19:52:00 -0700

    factorbot; product spec

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe('validations') do
+    it 'should require presence of name' do
+      product = build(:product, name: '')
+
+      expect{ product.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'should require presence of price' do
+      product = build(:product, price: '')
+
+      expect{ product.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
 end
